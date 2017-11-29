dc0f56eddf9f1caff7492288f29ddda494ae4531
Flux_Doctor <dj.kohal@yahoo.com>
2017-11-29 01:27:51 -0700

    building 1.0

---
+++ src/main/java/net/fluxdoctor/nullcraft/core/reference.java
@@ -0,0 +1,12 @@
+package net.fluxdoctor.nullcraft.core;
+
+public class reference
+
+{
+
+ public static final String MOD_ID = "nullcraft";
+ public static final String MOD_NAME = "NullCraft";
+ public static final String MOD_VERSION = "0.0.1a";
+
+
+}
